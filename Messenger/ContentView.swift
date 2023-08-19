2e4f3499427c2d346d5a4eefd3e3a05f6125f830
Edgar Vergara <edgarvm13@hotmail.com>
2023-08-18 19:08:20 -0600

    Initial Commit

---
+++ Messenger/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Messenger
+//
+//  Created by Edgar Vergara on 18/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Edgar Vergara on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
