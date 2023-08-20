46b7f4fe0cb48e8a2533b2791224776b98d6f87a
Edgar Vergara <edgarvm13@hotmail.com>
2023-08-20 15:38:11 -0600

    char view ui pt2

---
+++ Messenger/Core/Chat/View/ChatBubble.swift
@@ -0,0 +1,32 @@
+//
+//  ChatBubble.swift
+//  Messenger
+//
+//  Created by Edgar Vergara on 20/08/23.
+//
+
+import SwiftUI
+
+struct ChatBubble: Shape {
+    let isFromCurrentUser: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: [
+                .topLeft,
+                .topRight,
+                isFromCurrentUser ? .bottomLeft : .bottomRight
+            ],
+            cornerRadii: CGSize(width: 16, height: 16)
+        )
+        
+        return Path(path.cgPath)
+    }
+}
+
+struct ChatBubble_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatBubble(isFromCurrentUser: true)
+    }
+}
